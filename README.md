6d7c4c6da47553165545ae40ffe63d421d698e57
stepds <step.list+data.science@gmail.com>
2014-09-16 20:16:38 +0200

    Add support for desktop.ini and curl.exe.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ contrib-DataScienceSpecialization
 
 My contributions to the DataScienceSpecialization repository.
 
-* [start-RStudio.cmd](start-RStudio.cmd) - Window batch file to start RStudio from a folder. This script works around an RStudio startup issue, and sets the initial folder from which RStudio sources the .RData session initialization file. Instructions included in start-RStudio.cmd.
+* [start-RStudio.cmd](start-RStudio.cmd) - Window batch file to start RStudio from a folder. This script works around an RStudio startup issue, and sets the initial folder from which RStudio sources the .RData session initialization file. Instructions included in start-RStudio.cmd. **Updated 20140914.**
--- start-RStudio.cmd
+++ start-RStudio.cmd
@@ -4,8 +4,11 @@
 :: This file addresses the following cases:
 :: 1. At startup RStudio 0.98.994 (released July 2014) prints error messages
 ::    indicating that it can't load two of its libraries.
-:: 2. You want to preset RStudio's working directory to the folder where you
-::    keep your .RData and .R code files.
+:: 2. You want to preset the RStudio working directory to the folder where
+::    you keep your .RData and .R code files.
+:: 3. You want to run file.download() with method="curl" as typically used
+::    in the lecture notes.
+:: 4. You want to set the RStudio font family and size.
 :: 
 :: INSTALLATION
 :: In your DataScienceSpecialization folder create folders "setup" and
@@ -25,17 +28,61 @@
 ::    |__R-3.1.1-win.exe
 ::    |__RStudio-0.98.994.exe
 ::    |__start-RStudio.cmd
+::    |__desktop.ini   (see NOTES)
 :: 
 :: USAGE
 :: Open your "DataScienceSpecialization" folder. Go to folder "setup".
 :: If you need to install R and RStudio do so now.
 :: Double click file "start-RStudio.cmd" to launch RStudio.
 ::
+:: NOTES
+:: File desktop.ini is optional. If found it is copied to the
+:: RStudio folder in Windows user's profile folder to set the
+:: base font family and size.
+:: To customize this script look for tag CUSTOMIZE in the source file.
+::
 :: ^^^ Scroll up this window if you can't read the INSTRUCTIONS ^^^
 @echo on
 @"%SystemRoot%\system32\findStr.exe" /bl /c:"::" "%~dpnx0"
 @echo off
-pause
+echo Press any key to continue or Ctrl+C to terminate...
+pause >nul
+
+REM >>> CUSTOMIZE <<<
+REM Enable support for method="curl" in file.download().
+REM Download curl from http://curl.haxx.se/download.html.
+REM Get a win32-Generic or win64-Generic package, uncompress it in a folder
+REM of your choice, and append the folderpath to variable PATH as shown below.
+set PATH=%PATH%;P:\tipi\bin\curl
+
+REM Copy RStudio desktop.ini, if any, to Windows user's profile.
+REM You may remove "/-y " after "copy " to disable copy prompts.
+REM
+REM Note: The script attempts a two-way copy: first copy desktop.ini, if any,
+REM from the Windows profile folder to this folder for backup purposes;
+REM then copy desktop.ini from this folder to the Windows profile folder to
+REM initialize RStudio. If you have just installed RStudio the Windows profile
+REM folder is empty, so the first copy is silently skipped.
+set p="%USERPROFILE%\Application Data\RStudio\desktop.ini"
+set h="%~dp0desktop.ini"
+set t="%TEMP%\%~n0-%RANDOM%.tmp"
+if not exist %p% goto :files_are_different
+if not exist %h% goto :files_are_different
+"%SystemRoot%\system32\fc.exe" /u %p% %h% > %t%
+if %errorlevel% == 0 goto :done_desktop_ini
+type %t%
+:files_are_different
+mkdir "%USERPROFILE%\Application Data\RStudio"
+REM save profile to here on restarting
+if exist %p% ( echo [GET PROFILE] & copy /-y %p% %h% )
+REM save here to profile on starting
+if exist %h% ( echo [PUT PROFILE] & copy /-y %h% %p% )
+goto :done_desktop_ini
+:done_desktop_ini
+if exist %t% del %t%
+set p=
+set h=
+set t=
 
 REM Fix RStudio 0.98.994 unable to load its two libraries when
 REM the system command shell isn't cmd.exe.
